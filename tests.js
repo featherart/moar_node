f160e7a62d48db5422f816f1c4fc4dad9a00dfe8
Feather <lisafeather@gmail.com>
2014-07-02 17:26:55 -0700

    understanding fundamentals

--- tests.js
+++ tests.js
@@ -1,2 +1,8 @@
+var util = require('util');
+
 var a = {1: true, 2: false};
 console.log(a);
+
+console.log('this is a number: %d, and this is a string: %s, and this is an object in json: %j', 42, 'jasmine', a);
+
+console.log(util.inspect(a));
